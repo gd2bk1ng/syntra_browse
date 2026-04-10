53ef9c99d1b185f2a5659c1ea4044f0c14538f8a
GD2BK1NG <147461464+gd2bk1ng@users.noreply.github.com>
2026-04-10 11:45:43 +0000

    Create sandbox.rs
    Signed-off-by: GD2BK1NG <147461464+gd2bk1ng@users.noreply.github.com>

---
+++ src/security/sandbox.rs
@@ -0,0 +1,9 @@
+// file: src/security/sandbox.rs
+// Author: Alexandr Roussinov (gd2bk1ng)
+
+#[derive(Debug, Clone, Default)]
+pub struct Sandbox;
+
+impl Sandbox {
+  pub fn new() -> Self { Self }
+}
