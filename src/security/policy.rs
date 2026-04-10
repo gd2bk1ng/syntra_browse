4e259b19e31334dc9d79346c5f0870866f480459
GD2BK1NG <147461464+gd2bk1ng@users.noreply.github.com>
2026-04-10 11:42:05 +0000

    Create policy.rs
    Signed-off-by: GD2BK1NG <147461464+gd2bk1ng@users.noreply.github.com>

---
+++ src/security/policy.rs
@@ -0,0 +1,14 @@
+// file: src/security/policy.rs
+// Author: Alexandr Roussinov (gd2bk1ng)
+
+use crate::security::capability::Capability;
+
+#[derive(Debug, Clone)]
+pub enum Permission { Allow, Deny }
+
+#[derive[Debug, Clone, Default)]
+pub struct Policy;
+
+impl Policy {
+  pub fn evaluate(&self, _cap: &Capability) -> Permission { Permission::Allow }
+}
